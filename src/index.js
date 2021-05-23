1cfbd06d1d7df7b1393c57565fc1111e00336292
aisyah-h <83572065+aisyah-h@users.noreply.github.com>
2021-05-23 14:47:53 +0800

    Playing around with different ways of using curly braces curly braces: to input JavaScript in JSX

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Angela";
-const lucky = 8;
+const fName = "Angela";
+const lName = "Yu";
+const num = 8;
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>
-    <p>Your lucky number is: {lucky}.</p>
+    {/*3 ways to use variables in the html tag:*/}
+    {/*Method 1:*/}
+    <h1>Hello {fName + " " + lName}!</h1>
+    <p>Your lucky number is: {Math.floor(Math.random() * 10)}.</p>
+
+    {/*Method 2: using more than 1 set of curly braces*/}
+    <h1>
+      Hello {fName} {lName}!
+    </h1>
+    <p>Your lucky number is: {Math.floor(Math.random() * 10)}.</p>
+
+    {/*Method 3: Template Literal/String Interpolation 
+    (avoid this though, just makes the code complicated)
+    */}
+    <h1>Hello {`${fName} ${lName}`}!</h1>
+    <p>Your lucky number is: {Math.floor(Math.random() * 10)}.</p>
   </div>,
   document.getElementById("root")
 );
